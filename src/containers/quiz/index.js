90533f118cf43d29cbbe6d044194cf7c927668c2
Eric Freeman <clownhunter@gmail.com>
2016-10-24 21:25:58 -0600

    Present questions one at a time to player and keep score of how many points they have.

--- src/containers/quiz/index.js
+++ src/containers/quiz/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { getQuestions } from '../../modules/questions'; 
+import { getQuestions } from '../../modules/questions';
+import Question from './question';
 
 class Quiz extends React.Component {
     constructor(props) {
@@ -10,7 +11,10 @@ class Quiz extends React.Component {
         this.state = {
             isLoading: false,
             isError: false,
-            questions: []
+            questions: [],
+            currentQuestion: 0,
+            score: 0,
+            canProceed: false
         };
     }
 
@@ -19,22 +23,49 @@ class Quiz extends React.Component {
     }
 
     render() {
+        const { questions, isLoading, isError } = this.props;
+        const { currentQuestion, score, canProceed } = this.state;
+
         return (
             <div>
-                <h1>quiz</h1>
-                <p>
-                    { JSON.stringify(this.props.questions) }
-                </p>
+                <h1>Quiz</h1>
+                <h2>Score: { score }</h2>
+                { isLoading && <h3>Hold up while your quiz loads</h3> }
+                { isError && <h3>ERROR :(</h3> }
+                { questions.length > 0 &&
+                    <Question question={questions[currentQuestion]} answerSelected={this._answerSelected.bind(this)} />
+                }
+                { canProceed && <button onClick={this._nextQuestion}>Next Question</button> }
             </div>
         );
     }
+
+    _answerSelected = (isCorrect) => {
+        const { currentQuestion } = this.state;
+        const { questions } = this.props;
+
+        const score = this.state.score + (isCorrect ? 1 : 0);
+        this.setState({
+            score,
+            canProceed: currentQuestion < questions.length - 1
+        });
+    }
+
+    _nextQuestion = () => {
+        this.setState({
+            currentQuestion: this.state.currentQuestion + 1,
+            canProceed: false
+        })
+    }
 }
 
-const mapStateToProps = state => ({
-    isLoading: state.questions.isLoading,
-    isError: state.questions.isError,
-    questions: state.questions.questions
-});
+function mapStateToProps(state) {
+    return {
+        isLoading: state.questions.isLoading,
+        isError: state.questions.isError,
+        questions: state.questions.questions
+    };
+};
 
 const mapDispatchToProps = dispatch => bindActionCreators({
     getQuestions
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { ConnectedRouter } from 'react-router-redux'
 import store, { history } from './store'
 import App from './containers/app'
 
-import './index.css'
-
 const target = document.querySelector('#root')
 
 render(
